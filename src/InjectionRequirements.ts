67028acb534240edd49da3f08799e09e62410be3
Tremho <steve@ohmert.com>
2021-08-21 09:57:24 -0700

    another full test run

--- src/InjectionRequirements.ts
+++ src/InjectionRequirements.ts
@@ -1,10 +1,11 @@
 import * as nscore from '@nativescript/core'
 import {Application} from '@nativescript/core'
 import {registerExtensionModule} from "./api/BackExtensions";
-
+import * as startupTasks from './StartupTasks'
 const injections = {
     nscore,
     nativescriptApp:Application,
+    startupTasks,
     registerExtensionModule
 }
 export default injections
---
+++ src/StartupTasks.ts
@@ -0,0 +1,85 @@
+
+import * as nsfs from '@nativescript/core/file-system'
+import {FileSystemEntity} from "@nativescript/core/file-system";
+import {Device} from '@nativescript/core'
+import {Application} from '@nativescript/core'
+
+let passedEnvironment = {}
+
+export function readBuildEnvironment() {
+    let be = {}
+
+    console.log('>>$$$$ in readBuildEnvironment ')
+
+    const cwd = nsfs.knownFolders.currentApp().path
+    console.log('ns cwd (app) ', cwd)
+    const files = nsfs.Folder.fromPath(cwd).getEntitiesSync()
+    console.log('%%%%%%%%%%%%%%%%%%%%%%%%%')
+    console.log('contents at app root:')
+    for(let i=0; i<files.length; i++) {
+        const f = files[i]
+        const fileName = f.path.substring(f.path.lastIndexOf('/'))
+        console.log(`${i+1} - ${fileName})`)
+    }
+    console.log('%%%%%%%%%%%%%%%%%%%%%%%%%')
+
+
+    let beFile ='BuildEnvironment.json'
+    let text = ''
+
+    beFile = nsfs.knownFolders.currentApp().path+'/'+beFile
+
+    let exists = nsfs.File.exists(beFile);
+    console.log(beFile + ' exists? ', exists)
+    if(exists) {
+        try {
+            text = nsfs.File.fromPath(beFile).readTextSync()
+            if (text) {
+                be = JSON.parse(text)
+            } else {
+                console.error(beFile + ' Does not exist')
+                be = {
+                    error: "Unable to locate " + beFile,
+                }
+            }
+        } catch (e) {
+            console.error('Unable to read ' + beFile, e)
+            be = {
+                error: "Unable to read " + beFile,
+                errMsg: e.message
+            }
+        }
+    }
+    console.log('returning build environment data as ', be)
+    return mergeRuntimeInformation(be)
+
+}
+function mergeRuntimeInformation(buildEnv:any) {
+
+    const env = {
+        build: buildEnv,
+        runtime: {
+            framework: {
+                nativeScript: buildEnv.framework.nativeScript            },
+            platform: {
+                type: Device.deviceType || 'Mobile device',
+                name: 'NativeScript',
+                version: buildEnv.platform.nativeScript,
+                host: Device.os, // host OS (if nativescript)
+                hostVersion: Device.osVersion, // (if nativescript)
+                manufacturer: Device.manufacturer // (if nativescript)
+            }
+        }
+    }
+    passedEnvironment = env
+    return env
+}
+
+export function passEnvironmentAndGetTitles(): { appName:string, title:string } {
+    Application.setResources({passedEnvironment:passedEnvironment})
+
+    let env:any = passedEnvironment
+    let appName = (env.build.app && env.build.app.name) || 'jove-app'
+    let title = env.build.app.displayName || appName
+    return {appName, title}
+}
