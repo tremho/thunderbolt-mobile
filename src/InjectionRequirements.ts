a914f373bb9b5076de79367707877753a29321cb
Tremho <steve@ohmert.com>
2021-08-21 12:57:15 -0700

    another full test run

--- src/InjectionRequirements.ts
+++ src/InjectionRequirements.ts
@@ -2,10 +2,13 @@ import * as nscore from '@nativescript/core'
 import {Application} from '@nativescript/core'
 import {registerExtensionModule} from "./api/BackExtensions";
 import * as startupTasks from './StartupTasks'
+import ComponentBase from "./mobile/ComponentBase"
+
 const injections = {
     nscore,
     nativescriptApp:Application,
     startupTasks,
+    ComponentBase,
     registerExtensionModule
 }
 export default injections
