e09c733c2a91eb511d76707e6ff5bd5f9ba6921f
Tremho <steve@ohmert.com>
2021-10-01 10:11:42 -0700

    try removing websend

--- src/api/ApiGateway.ts
+++ src/api/ApiGateway.ts
@@ -1,10 +1,8 @@
 
 import * as fileApi from './FileApi'
 import * as dialogApi from './DialogAPI'
-import * as webApi  from './WebApi'
 
 export const mainApi = {
     ...fileApi,
-    ...dialogApi,
-    ...webApi
+    ...dialogApi
 }
