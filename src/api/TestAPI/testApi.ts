b13ac50976f980cddf45fabd8feea5e746ec79ea
Tremho <steve@ohmert.com>
2021-11-09 12:32:07 -0800

    add nativescript test launch

--- src/api/TestAPI/testApi.ts
+++ src/api/TestAPI/testApi.ts
@@ -7,16 +7,19 @@ function setTestStatus(inStatus:any) {
     testStatus = JSON.stringify(inStatus)
 }
 
-export async function startTest(testPath:string = '/test') {
-    const client = new H2Client()
-    console.log('Client starting')
-    client.request(testPath)
-    let alive = true
-    while(alive) {
-        const code = await client.syncStatus(testStatus)
-        alive = await client.processDirectives()
-    }
-
-    console.log('done')
-    client.end()
+export async function startTest(host:string) {
+    console.log('Jove Mobile test host is', host)
+    let service = "ws://"+host+":51610"
+    console.log('  .... service is', service)
+    // const client = new H2Client()
+    // console.log('Client starting')
+    // client.request(testPath)
+    // let alive = true
+    // while(alive) {
+    //     const code = await client.syncStatus(testStatus)
+    //     alive = await client.processDirectives()
+    // }
+    //
+    // console.log('done')
+    // client.end()
 }
