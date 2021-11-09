40f71d761ca9f6672b0d2ebb88c8ac7cd634e98a
Tremho <steve@ohmert.com>
2021-11-09 14:17:13 -0800

    fixups around connect and close 2

--- src/api/TestAPI/testApi.ts
+++ src/api/TestAPI/testApi.ts
@@ -11,12 +11,5 @@ export async function startTest(host:string) {
     console.log('Jove Mobile test host is', host)
     let service = "ws://"+host+":51610"
     console.log('  .... service is', service)
-    return new Promise(resolve => {
-        let service = "ws://"+host+":51610"
-
-        clientTest(service).then((code:number) => {
-            console.log('done', code)
-            resolve(code)
-        })
-    })
+    return clientTest(service)
 }
