0b3530a788718a65e6c146cb57fd656c85a9fbf9
Tremho <steve@ohmert.com>
2021-11-10 14:29:49 -0800

    try alternative to timeout handling

--- src/api/TestAPI/TestActions.ts
+++ src/api/TestAPI/TestActions.ts
@@ -94,7 +94,12 @@ export async function callPageFunction(funcName:string, parameters:string[] = []
  * @param delay
  */
 export async function wait(delay:number) {
-    return new Promise(resolve => { setTimeout(resolve, delay)})
+    return new Promise(resolve => {
+        const timerId = setTimeout(() => {
+            clearTimeout(timerId)
+            resolve(delay)
+        }, delay)
+    })
 }
 
 // perform a menu action
