4ee6e99c0d1f5d020aa5f56d93dbc12f07db0bea
Tremho <steve@ohmert.com>
2021-11-13 13:59:30 -0800

    Mobile time is relative

--- src/api/TestAPI/TestActions.ts
+++ src/api/TestAPI/TestActions.ts
@@ -96,7 +96,7 @@ export async function callPageFunction(funcName:string, parameters:string[] = []
 export async function wait(delay:number) {
     // console.log('waiting for '+delay/1000+' seconds')
     // for whatever reason, we find this ends up as 9X longer than expected, so divide it down
-    delay = Math.floor(delay/7.0)
+    delay = Math.floor(delay/2.0)
     return new Promise(resolve => { setTimeout(resolve, delay)})
 }
 
