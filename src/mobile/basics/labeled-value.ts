d4d302a35a812f953620a82b37c1ec4cd93de781
Tremho <steve@ohmert.com>
2021-11-28 11:18:42 -0800

    labeled value

--- src/mobile/basics/labeled-value.ts
+++ src/mobile/basics/labeled-value.ts
@@ -5,6 +5,7 @@ import {Label, Color} from '@nativescript/core'
 
 export class LabeledValue extends ComponentBase {
     private cLabel:Label = new Label()
+    private cValue:Label = new Label()
 
     // Override to create our label
     public createControl() {
@@ -12,18 +13,24 @@ export class LabeledValue extends ComponentBase {
             // our component property identified by the key (text)
             // is represented by inner 'component' (this.label) at its property (text)
             // and its parseable value comes from 'value' {this.get('text')}
-            label: {component: this.cLabel, locprop: 'text', value: this.get('label')}
+            label: {component: this.cLabel, locprop: 'text', value: this.get('label')},
+            value: {component: this.cValue, locprop: 'text', value: this.get('value')},
         }
         this.cLabel.set('text', 'L-Value')
         this.cLabel.set("whiteSpace", "nowrap")
         this.cLabel.set("textWrap", false)
         this.textComponent = this.cLabel // so ComCommon will align text
+        this.cValue.set('text', 'L-Value')
+        this.cValue.set("whiteSpace", "nowrap")
+        this.cValue.set("textWrap", false)
         this.component.orientation = 'horizontal'
         this.component.addChild(this.cLabel)
+        this.component.addChild(this.cValue)
         // set the default alignment of component to the left. The {N} default appears to be center.
         this.cLabel.set('horizontalAlignment', 'left')
+        this.cValue.set('horizontalAlignment', 'left')
         this.set('horizontalAlignment', 'left')
-        if(this.get('action')) this.setActionResponder(this.cLabel, 'tap', 'action')
+        if(this.get('action')) this.setActionResponder(this.component, 'tap', 'action')
     }
 
 }
