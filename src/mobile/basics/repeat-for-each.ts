0bbe237f2281210cb1166342a6d58f990694c9ed
Tremho <steve@ohmert.com>
2021-12-08 12:38:36 -0800

    back to repeat-for-each mobile

--- src/mobile/basics/repeat-for-each.ts
+++ src/mobile/basics/repeat-for-each.ts
@@ -6,6 +6,11 @@ export class RepeatForEach extends ComponentBase {
     // Override to create our label
     public createControl() {
         console.log('>> repeat-for-each')
+        for(let p of Object.getOwnPropertyNames(this)) {
+            console.log('  p = ', this.get(p))
+
+        }
+        console.log("<<<<<")
     }
 
 }
