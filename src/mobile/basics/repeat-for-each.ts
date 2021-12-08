4fa74197005d9c5a83e958f59bea800f7aed84bd
Tremho <steve@ohmert.com>
2021-12-08 12:58:45 -0800

    back to repeat-for-each mobile

--- src/mobile/basics/repeat-for-each.ts
+++ src/mobile/basics/repeat-for-each.ts
@@ -2,14 +2,41 @@
 import ComponentBase from '../ComponentBase'
 import {Color} from "@nativescript/core";
 
+const ignoreProps = [
+    "pseudoClassAliases",
+    "cssClasses",
+    "cssPseudoClasses",
+    "textComponent",
+    "defaultProps",
+    "state",
+    "tagName",
+    "props",
+    "component",
+    "container",
+    "comNormal",
+    "parent",
+    "effectivePaddingTop",
+    "effectivePaddingRight",
+    "effectivePaddingBottom",
+    "effectivePaddingLeft",
+    "nativeViewProtected",
+    "layoutChangeListenerIsSet",
+    "layoutChangeListener",
+    "b",
+    "com",
+    "cm"
+]
+
 export class RepeatForEach extends ComponentBase {
     // Override to create our label
     public createControl() {
         console.log('>> repeat-for-each')
+        const props:any = {}
         for(let p of Object.getOwnPropertyNames(this)) {
-            console.log(`  ${p} = `, typeof this.get(p))
-
+            if(p.charAt(0) === '_' || ignoreProps.indexOf(p) !== -1) continue
+            props[p] = this.get(p)
         }
+        console.log('props', props)
         console.log("<<<<<")
     }
 
