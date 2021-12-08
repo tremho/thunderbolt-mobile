118607565d4a435ec11f7f3f5a0cd70cca15ca49
Tremho <steve@ohmert.com>
2021-12-08 10:53:27 -0800

    mobile hr

--- src/mobile/basics/hr.ts
+++ src/mobile/basics/hr.ts
@@ -1,17 +1,35 @@
 
 import ComponentBase from '../ComponentBase'
 
+import {Label, Color} from '@nativescript/core'
+
 export class Hr extends ComponentBase {
+    private label:Label = new Label()
 
     // Override to create our label
     public createControl() {
-        this.component.set('height', '10')
-        this.component.set('horizontalAlignment', 'center')
-        this.component.set('backgroundColor', 'black')
-        this.component.set('width', '100%')
-        this.component.set('height', '10')
-        this.component.className = 'hr'
+        console.log('>> hr setting other props')
+        this.label.set("whiteSpace", "nowrap")
+        this.label.set("textWrap", false)
+        this.textComponent = this.label // so ComCommon will align text
+        console.log('>> hr label added as child')
+        this.component.addChild(this.label)
+        // set the default alignment of component to the left. The {N} default appears to be center.
+        console.log('>> hr setting alignment')
+        this.label.set('horizontalAlignment', 'center')
+        console.log('>> hr setting color')
+        this.label.set('backgroundColor', 'black')
+        this.label.set('width', '100%')
+        this.label.set('height', '2')
+        this.label.className = 'hr'
+        this.set('horizontalAlignment', 'left')
+        if(this.get('action')) this.setActionResponder(this.label, 'tap', 'action')
     }
+
+    // protected setProperties() {
+    //     this.setDynamicExpressions(this.get('text') || '$text', this.label, 'text', 'text')
+    // }
+
 }
 
 
