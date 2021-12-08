ed47244b9774c288172d38d23eaeee79cf3feb4c
Tremho <steve@ohmert.com>
2021-12-08 11:05:49 -0800

    mobile hr

--- src/mobile/basics/hr.ts
+++ src/mobile/basics/hr.ts
@@ -1,29 +1,29 @@
 
 import ComponentBase from '../ComponentBase'
 
-import {Label, Color} from '@nativescript/core'
+// import {Label, Color} from '@nativescript/core'
 
 export class Hr extends ComponentBase {
-    private label:Label = new Label()
+    // private label:Label = new Label()
 
     // Override to create our label
     public createControl() {
-        console.log('>> hr setting other props')
-        this.label.set("whiteSpace", "nowrap")
-        this.label.set("textWrap", false)
-        this.textComponent = this.label // so ComCommon will align text
-        console.log('>> hr label added as child')
-        this.component.addChild(this.label)
+        // console.log('>> hr setting other props')
+        // this.label.set("whiteSpace", "nowrap")
+        // this.label.set("textWrap", false)
+        // this.textComponent = this.label // so ComCommon will align text
+        // console.log('>> hr label added as child')
+        // this.component.addChild(this.label)
         // set the default alignment of component to the left. The {N} default appears to be center.
         console.log('>> hr setting alignment')
-        this.label.set('horizontalAlignment', 'center')
-        this.label.set('verticalAlignment', 'top')
+        this.set('horizontalAlignment', 'center')
+        this.set('verticalAlignment', 'top')
         console.log('>> hr setting color')
-        this.label.set('backgroundColor', 'black')
-        this.label.set('width', '100%')
-        this.label.set('height', '2')
-        this.label.className = 'hr'
-        this.set('horizontalAlignment', 'left')
+        this.set('backgroundColor', 'black')
+        this.set('width', '100%')
+        this.set('height', '2')
+        this.className = 'hr'
+        // this.set('horizontalAlignment', 'left')
         console.log('>> hr done')
     }
 
