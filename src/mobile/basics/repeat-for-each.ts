8a5143767e1b68a73c1c82cf5211109f6178615a
Tremho <steve@ohmert.com>
2021-12-08 13:31:40 -0800

    back to repeat-for-each mobile

--- src/mobile/basics/repeat-for-each.ts
+++ src/mobile/basics/repeat-for-each.ts
@@ -1,6 +1,6 @@
 
 import ComponentBase from '../ComponentBase'
-import {Color} from "@nativescript/core";
+import {View} from "@nativescript/core";
 
 const ignoreProps = [
     "pseudoClassAliases",
@@ -28,24 +28,45 @@ const ignoreProps = [
 ]
 
 export class RepeatForEach extends ComponentBase {
+    private slots:View[] | null = null
     // Override to create our label
     public createControl() {
         console.log('>> repeat-for-each')
         const props:any = {}
+        let subject:any[] = []
         for(let p of Object.getOwnPropertyNames(this)) {
             if(p.charAt(0) === '_' || ignoreProps.indexOf(p) !== -1) continue
-            props[p] = this.get(p)
+            if(p === 'subject') subject = this.get(p)
+            else props[p] = this.get(p)
         }
         console.log('props', props)
 
-        // clear all children
+        // clear all children / collect slot children
+        let collecting = false
+        if(!this.slots) {
+            this.slots = []
+            collecting = true
+        }
         let n = this.getChildrenCount();
         while(--n >=0) {
             let ch = this.getChildAt(n)
             console.log('existing child', ch)
+            if(collecting) this.slots.push(ch)
             this.removeChild(ch)
         }
-        // then add them
+        // then add the children back per repeat
+        for(let item of subject) {
+            for(let c of this.slots) {
+                let cname = c.constructor.name
+                let cprops:any = {}
+                for(let p of Object.getOwnPropertyNames(c)) {
+                    if(p.charAt(0) === '_' || ignoreProps.indexOf(p) !== -1) continue
+                    cprops[p] = c.get(p)
+                }
+                console.log('> we want to create ', cname, 'with', cprops)
+
+            }
+        }
 
 
         console.log("<<<<<")
