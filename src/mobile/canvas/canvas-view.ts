f773c4dc6feef38536c594f8b2629adc89a7fcce
Tremho <steve@ohmert.com>
2022-02-27 10:25:04 -0800

    Canvas (NS) refactored as pass-thru

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -1,76 +1,14 @@
 
-import ComponentBase from '../ComponentBase'
-
-import {Label, Color} from '@nativescript/core'
-
 let Canvas:any
 try {
     Canvas = require('@nativescript/canvas').Canvas
 } catch(e) {
 }
 
-export class CanvasView extends ComponentBase {
-    // @ts-ignore
-    private canvas:any = null;
-    private ctx:any = null
-    private contextType:string = ''
-
-    constructor() {
-        super()
-        if(Canvas) {
-            this.canvas = new Canvas()
-        } else {
-            throw Error('@nativescript/canvas (plugin) is not installed')
-        }
-        this.component.addChild(this.canvas)
-    }
-
-    // Override to create our control
-    public createControl() {
-        this.className = "CanvasView"
-        let size = this.getProp('size')
-        const rect = this.getElementBounds(this.component)
-        if(!size) {
-            size = (rect.height || rect.width)
-        }
-        let sizeH = this.isAndroid ? size * 0.75 : size
-        this.ctx = this.getContext()
-        if(this.ctx) {
-            this.ctx.canvas.width = size
-            this.ctx.canvas.height = sizeH
-        }
-        if(this.get('action')) this.setActionResponder(this.canvas, 'tap', 'action')
-
-    }
-    public getContext(type?:string) {
-        if(!this.ctx || type) {
-            if (!type) type = this.getProp('type') || 'none'
-            type = (type && type.toLowerCase()) || ''
-            if (type === '2d' || type === 'webgl' || type === '3d') {
-                if(type === '3d') type = 'webgl' // 3d is synonymous with webGL 1.0
-                try {
-                    let rctx = this.canvas.getContext(type)
-                    this.ctx = rctx
-                    if(this.ctx) {
-                        this.contextType = type
-                    } else {
-                    }
-
-                } catch (e) {
-                    console.error('Canvas initialization has failed')
-                    throw e;
-                }
-            } else {
-                throw Error('canvas type must be "2d" or "webgl"')
-            }
-        }
-        return this.ctx
-    }
-
-    // protected setProperties() {
-    //     this.setDynamicExpressions(this.get('text') || '$text', this.label, 'text', 'text')
-    // }
-
+// the canvas-view is not a normal ComponentBase like the other Jove controls
+// instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
+// it will call canvasReady
+export class CanvasView extends Canvas {
 }
 
 
