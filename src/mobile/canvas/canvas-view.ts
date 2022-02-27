bdc941e10492d292affd4bbf902f7932ed6f71a8
Tremho <steve@ohmert.com>
2022-02-27 13:33:33 -0800

    New attempt to trap canvas events to activity

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -1,3 +1,4 @@
+import {EventData, getTheApp} from "../ComponentBase";
 
 let Canvas:any
 try {
@@ -9,6 +10,35 @@ try {
 // instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
 // it will call canvasReady
 export class CanvasView extends Canvas {
+    constructor() {
+        super()
+        this.on('ready', (ev:any) => {
+            this.respondToAction(this, ev,'ready', 'ready')
+        })
+        this.on('tap', (ev:any) => {
+            this.respondToAction(this, ev,'tap', 'action')
+        })
+    }
+    protected respondToAction(view:any, event:any, eventName:string, tag:string = 'action', value?:any) {
+        const target = this.get(tag)
+        const ed = new EventData()
+        const app = getTheApp()
+        ed.app = app
+        ed.sourceComponent = this
+        ed.eventType = eventName
+        ed.tag = tag
+        ed.value = value
+        // console.log('Event occurs', eventName)
+        ed.platEvent = event
+        // console.log('>>>>>>>>>>>> getting activity from app', app, ed.app)
+        const activity = app.currentActivity
+        // console.log('activity found', activity)
+        // console.log('should call '+target)
+        if(activity && typeof activity[target] === 'function') {
+            activity[target](ed)
+        }
+    }
+
 }
 
 
