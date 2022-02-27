cdcea0d2a5e59d3ac369b7ed2a52e3b29d335f0a
Tremho <steve@ohmert.com>
2022-02-27 14:14:52 -0800

    Reverting to what was working, however imperfectly

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -8,37 +8,8 @@ try {
 
 // the canvas-view is not a normal ComponentBase like the other Jove controls
 // instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
-// it will call canvasReady
+// it will call canvasReady (on the page, not the activity, so this is forwarded by the page stubs)
 export class CanvasView extends Canvas {
-    constructor() {
-        super()
-        this.on('ready', (ev:any) => {
-            this.respondToAction(this, ev,'ready', 'ready')
-        })
-        this.on('tap', (ev:any) => {
-            this.respondToAction(this, ev,'tap', 'action')
-        })
-    }
-    protected respondToAction(view:any, event:any, eventName:string, tag:string = 'action', value?:any) {
-        const target = this.get(tag)
-        const ed = new EventData()
-        const app = getTheApp()
-        ed.app = app
-        ed.sourceComponent = this
-        ed.eventType = eventName
-        ed.tag = tag
-        ed.value = value
-        // console.log('Event occurs', eventName)
-        ed.platEvent = event
-        // console.log('>>>>>>>>>>>> getting activity from app', app, ed.app)
-        const activity = app.currentActivity
-        // console.log('activity found', activity)
-        // console.log('should call '+target)
-        if(activity && typeof activity[target] === 'function') {
-            activity[target](ed)
-        }
-    }
-
 }
 
 
