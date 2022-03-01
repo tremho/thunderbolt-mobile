1f9d6ecf6704482988dd831dd892fb36175bbd39
Tremho <steve@ohmert.com>
2022-03-01 14:15:26 -0800

    putting utility into util usage

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -12,14 +12,14 @@ try {
 // instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
 // it will call canvasReady (on the page, not the activity, so this is forwarded by the page stubs)
 export class CanvasView extends Canvas {
-    private util:ComponentBase  // we use this just for gesture attachment...
+    // private util:ComponentBase  // we use this just for gesture attachment...
 
-    constructor() {
-        super()
-        let cv = this as unknown as View
-        this.util = new ComponentBase(cv)
-        this.util.listenToAllGestures(cv, 'action')
-    }
+    // constructor() {
+    //     super()
+    //     let cv = this as unknown as View
+    //     this.util = new ComponentBase(cv)
+    //     this.util.listenToAllGestures(cv, 'action')
+    // }
 }
 
 
