39b8b74247138146f5bd03c4c1c6c2fa11060c6d
Tremho <steve@ohmert.com>
2022-03-01 08:35:32 -0800

    alternate treatment for pan on event 128

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -8,7 +8,7 @@ try {
 } catch(e) {
 }
 
-// the canvas-view is not a normal ComponentBase like the other Jove controls
+// the canvas-view is not a normal ComponentBase like the other Jove controls...
 // instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
 // it will call canvasReady (on the page, not the activity, so this is forwarded by the page stubs)
 export class CanvasView extends Canvas {
