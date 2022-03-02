6af83c14bfee795138f19a3bbb9b72b36d2c7565
Tremho <steve@ohmert.com>
2022-03-02 10:10:01 -0800

    stopped working again -- 10:10

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -12,19 +12,14 @@ try {
 // instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
 // it will call canvasReady (on the page, not the activity, so this is forwarded by the page stubs)
 export class CanvasView extends Canvas {
+    private util:ComponentBase  // we use this just for gesture attachment
 
-    // Canvas uses a DOM event model rather than the NS gesture patterns
-    // So we don't connect to it as a listener.
-    // To attach UX to a canvas, wrap it in a StackLayout or other container and listen there.
-
-    // private util:ComponentBase  // we use this just for gesture attachment
-    //
-    // constructor() {
-    //     super()
-    //     let cv = this as unknown as View
-    //     this.util = new ComponentBase(cv)
-    //     this.util.listenToAllGestures(cv, 'action')
-    // }
+    constructor() {
+        super()
+        let cv = this as unknown as View
+        this.util = new ComponentBase(cv)
+        this.util.listenToAllGestures(cv, 'action')
+    }
 }
 
 
