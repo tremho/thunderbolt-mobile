da4fd3efd1ed818c5186b7692b62839184d0643c
Tremho <steve@ohmert.com>
2022-03-03 09:38:35 -0800

    Refactored to ns centric handlers -- 9:37am

--- src/mobile/canvas/canvas-view.ts
+++ src/mobile/canvas/canvas-view.ts
@@ -1,4 +1,4 @@
-// To use this component, the @nativescript/canvas plugin must be installed
+// To use this component, the @nativescript/canvas plugin must be installed.
 
 let Canvas:any
 try {
@@ -6,8 +6,8 @@ try {
 } catch(e) {
 }
 
-// the canvas-view is not a normal ComponentBase like the other Jove controls
-// instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
+// the canvas-view is not a normal ComponentBase like the other Jove controls.
+// Instead, it is simply a renamed version of the @nativescript/canvas directly (similar to stack-layout).
 // it will call canvasReady (on the page, not the activity, so this is forwarded by the page stubs)
 // to trap gestures, add a listener to the surrounding stack-layout
 export class CanvasView extends Canvas {
