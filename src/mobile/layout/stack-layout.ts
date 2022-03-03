db2bb7a3a7c95004c7ffb83229d1e350bc03f9dd
Tremho <steve@ohmert.com>
2022-03-03 10:00:09 -0800

    Refactored to ns centric handlers -- 9:59am

--- src/mobile/layout/stack-layout.ts
+++ src/mobile/layout/stack-layout.ts
@@ -6,13 +6,16 @@ export class TBStackLayout extends StackLayout {
     private util:ComponentBase
     constructor() {
         super()
-        let cv = this as unknown as View
-        this.util = new ComponentBase(cv)
+
         this.set('height', this.get('height') || '100%')
         this.set('width', this.get('width') || '100%')
         this.set('horizontalAlignment', this.get('horizontalAlignment') || 'left')
         this.on('layoutChanged', () => {
-            this.util.listenToAllGestures(cv, 'action')
+            let cv = this as unknown as View
+            if(!this.util) { // do only once
+                this.util = new ComponentBase(cv)
+                this.util.listenToAllGestures(cv, 'action')
+            }
         })
     }
 }
